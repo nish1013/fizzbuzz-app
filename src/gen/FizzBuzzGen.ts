a5829e07865a58a5348a606956d811e2136fe90d
nish1013 <nishand2d@gmail.com>
2023-07-20 12:44:47 +0100

    For numbers that are multiples of both three and five print “FizzBuzz”.

--- src/gen/FizzBuzzGen.ts
+++ src/gen/FizzBuzzGen.ts
@@ -3,7 +3,9 @@ import { Singleton } from "typescript-ioc";
 @Singleton
 export class FizzBuzzGen {
     generate(num: number): string {
-        if (num % 3 === 0) {
+        if (num % 3 === 0 && num % 5 === 0) {
+            return "FizzBuzz";
+          } else if (num % 3 === 0) {
             return "Fizz";
           } else if (num % 5 === 0) {
             return "Buzz";
