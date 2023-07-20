529f5aa0cae71c98e89fa8fa5bec622bdc46cfc2
nish1013 <nishand2d@gmail.com>
2023-07-20 12:06:06 +0100

    For multiples of three print “Fizz” instead of the number.

--- tests/FizzBuzzGen.spec.ts
+++ tests/FizzBuzzGen.spec.ts
@@ -8,4 +8,10 @@ describe("FizzBuzzGen", () => {
     expect(fizzBuzzGen.generate(10)).to.equal("10");
     expect(fizzBuzzGen.generate(100)).to.equal("100");
   });
+  it("For multiples of three print “Fizz” instead of the number.", () => {
+    const fizzBuzzGen = new FizzBuzzGen();
+    expect(fizzBuzzGen.generate(3)).to.equal("Fizz");
+    expect(fizzBuzzGen.generate(6)).to.equal("Fizz");
+    expect(fizzBuzzGen.generate(9)).to.equal("Fizz");
+  });
 });
